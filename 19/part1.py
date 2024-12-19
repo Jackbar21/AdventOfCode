73cde71cbb326874a2c797f860601ef2554581e9
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-18 23:23:52 -0600

    19

---
+++ 19/part1.py
@@ -0,0 +1,39 @@
+from functools import cache
+
+
+USE_TEST_DATA = False
+
+file_name = "./data.txt" if not USE_TEST_DATA else "./test_data.txt"
+with open(file_name, "r") as file:
+    #########################
+    ### START PARSE INPUT ###
+    #########################
+    lines = [line.strip() for line in file.readlines()]
+    patterns = lines[0].split(", ")
+    designs = lines[2:]
+    #######################
+    ### END PARSE INPUT ###
+    #######################
+
+    # TODO: Consider using full string instead
+    # def dp(design, i = 0):
+    #     if i >= len(design):
+    #         return True
+    @cache
+    def dp(design: str) -> bool:
+        if len(design) == 0:
+            return True
+        
+        for pattern in patterns:
+            if design.startswith(pattern):
+                if dp(design[len(pattern):]):
+                    return True
+
+        return False
+        
+
+    res = 0
+    for design in designs:
+       res += dp(design)
+
+    print(f"ANSWER: {res}")
