046bc8c3735f42535024681a05b4d0185a3f37a9
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-21 13:48:25 -0600

    20 - part2 cleanup

--- 20/part2.py
+++ 20/part2.py
@@ -1,13 +1,5 @@
 import collections
-import heapq
-import time
-
-
-class Node:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
+from functools import cache
 
 USE_TEST_DATA = False
 
@@ -15,8 +7,6 @@ file_name = "./test_data.txt" if USE_TEST_DATA else "./data.txt"
 with open(file_name, "r") as file:
     lines = [line.strip() for line in file.readlines()]
     grid = [[c for c in line] for line in lines]
-    # for line in grid:
-    #     # print("".join(line))
 
     START, END, TRACK, WALL = "S", "E", ".", "#"
     M, N = len(grid), len(grid[0])
@@ -31,162 +21,85 @@ with open(file_name, "r") as file:
     start, end = None, None
     for i in range(M):
         for j in range(N):
-            if grid[i][j] == WALL:
-                continue
-
             if grid[i][j] == START:
                 start = (i, j)
             if grid[i][j] == END:
                 end = (i, j)
+    # assert start is not None and end is not None
+
+    @cache
+    def bfs(i, j):
+        fringe = collections.deque([(0, i, j)]) # (cost, x, y)
+        visited = set()
+        visited.add((i, j))
+        while len(fringe) > 0:
+            cost, x, y = fringe.popleft()
+
+            if (x, y) == end:
+                return cost
 
             for dx, dy in DIRECTIONS:
-                x, y = i + dx, j + dy
-                if inBounds(x, y):
-                    if grid[x][y] != WALL:
-                        adj_list[(i, j)].append((1, None, x, y))
-                        # adj_list[(i, j)].append((None, x + M, y + N))
-                        adj_list[(i + M, j + N)].append((1, None, x + M, y + N))
+                new_x, new_y = x + dx, y + dy
+                if (new_x, new_y) not in visited and grid[new_x][new_y] != WALL:
+                    visited.add((new_x, new_y))
+                    fringe.append((cost + 1, new_x, new_y))
 
-                    # No matter what, even if (x, y) is a wall, we can traverse THROUGH IT
-                    # into "clonged graph". This is Vassos Hadzilacos trick from UofT ;)
-                    if True:
-                        # Add as a neighbor, every square that is at most 20 away from (i, j), and ends at a non-wall position
-                        queue = collections.deque(
-                            [(1, x, y)]
-                        )  # (path_len, cheat_x, cheat_y)
-                        vis = set()
-                        vis.add((i, j))
-                        vis.add((x, y))
-                        while len(queue) > 0:
-                            path_len, cheat_x, cheat_y = queue.popleft()
-                            if path_len > 20:
-                                continue  # Invalid!
-                            if (
-                                inBounds(cheat_x, cheat_y)
-                                and grid[cheat_x][cheat_y] != WALL
-                            ):  # Last picosecond of cheat can NOT be at a wall!!!
-                                adj_list[(i, j)].append(
-                                    (
-                                        path_len,
-                                        f"{i, j}-->{cheat_x, cheat_y}",
-                                        cheat_x + M,
-                                        cheat_y + N,
-                                    )
-                                )
-
-                            for dx, dy in DIRECTIONS:
-                                new_x, new_y = cheat_x + dx, cheat_y + dy
-                                if inBounds(new_x, new_y) and (new_x, new_y) not in vis:
-                                    vis.add((new_x, new_y))
-                                    queue.append((path_len + 1, new_x, new_y))
-
-                        # for delta_x, delta_y in DIRECTIONS:
-                        #     cheat_x, cheat_y = (x + delta_x, y + delta_y)
-                        #     if inBounds(cheat_x, cheat_y) and grid[cheat_x][cheat_y] != WALL and (cheat_x, cheat_y) != (i, j):
-                        #         # First element is the UNIQUE cheat of this path! I.e. similar to where they place
-                        #         # '1' and '2' in problem description :)
-                        #         # adj_list[(i, j)].append((2, f"{x + M, y + N}-->{cheat_x + M, cheat_y + N}", cheat_x + M, cheat_y + N))
-                        #         adj_list[(i, j)].append((2, f"{i, j}-->{cheat_x + M, cheat_y + N}", cheat_x + M, cheat_y + N))
-
-    # For graph G', this will represent essentially a clone of original graph,
-    # but now for every path u->v, we'll introduce an edge from u->v' of cost 1,
-    # regardless of whether position v is a wall or not! For every node u with
-    # coordinates (i, j), we'll denote u' with coordinates (i + M, j + N)
-    # # print(f"{adj_list=}")
-    # exit()
-    # for i in range(M):
-    #     for j in range(N):
-
-    assert start is not None and end is not None
-    GOAL_STATE = (end[0] + M, end[1] + N)
-    SHORTEST_PATH = 84 if USE_TEST_DATA else 9484
+    SHORTEST_PATH = bfs(start[0], start[1])
     GOAL_COST = SHORTEST_PATH - (100 if not USE_TEST_DATA else 50)
-    # # print(f"{adj_list=}")
 
-    banned = set()
-
-    res = []
-    fringe = [(0, start, None)]  # (cost, node, cheat_node)
-    # fringe = collections.deque(fringe)
+    # Idea: BFS our way through EEEEEEEEEVERY track inside of the graph, keeping track of the cost it's taken to get there
+    # (and marking as visited for the future). From each uniquely visited track, grab all of it's "cheatable" neighbors and
+    # then dictate the cheat as valid if and only if the current cost, plus the cost of the cheat, plus the cost remaining
+    # to the end node without cheating, is ALL <= GOAL_COST (i.e. shortest original path cost - 100 for real input data)
+    found_cheats = set()
+    fringe = collections.deque([(0, start[0], start[1])]) # (cost, x, y)
     visited = set()
-    START_TIME = time.time()
+    visited.add((start[0], start[1]))
     while len(fringe) > 0:
-        # print(f"{fringe=}")
-        cost, node, cheat_node = heapq.heappop(fringe)
-        assert cheat_node is None or len(cheat_node) > 0
-        # cost, node, cheat_node = fringe.popleft()
-
-        if node in visited:
+        cost, x, y = fringe.popleft()
+        if cost > GOAL_COST:
             continue
-        visited.add(node)
-        # if node[0] < M:
-        #     visited.add(node)
+        print(f"{len(found_cheats)=}")
 
-        # # print(f"{node=}")
-        # # print(f"{adj_list[node]}")
-        if node == GOAL_STATE:
-            # # print(f"SHORTEST PATH: {cost}")
-            # # print(f"{M=}, {N=}")
-            if cost > GOAL_COST:
-                print(f"ANSWER: {len(banned)}")
-                # print(f"{banned=}")
-                print(f"VASSOS TOTAL TIME: {time.time() - START_TIME}")
-                exit()
-            else:
-                fringe = [(0, start, None)]
-                visited.clear()
-                assert cheat_node is not None
-                assert cheat_node not in banned
-                banned.add(cheat_node)
-                print(f"{len(banned)=}")
+        # No termination case, want to visit ALLLLLLLLL of the TRACKS!!!!
+        for dx, dy in DIRECTIONS:
+            neigh_x, neigh_y = x + dx, y + dy
+            # assert inBounds(neigh_x, neigh_y)
+            if (neigh_x, neigh_y) in visited or grid[neigh_x][neigh_y] == WALL:
                 continue
-            # visited.remove(GOAL_STATE)
+            visited.add((neigh_x, neigh_y))
+            fringe.append((cost + 1, neigh_x, neigh_y))
+        
+        # Now, check for ALL the cheats from this starting position! If any of them yield a valid solution, i.e. have
+        # total cost <= GOAL_COST, add them to found_cheats set :) These should NOT be added to fringe for visitation,
+        # the results should be immediately calculated from THIS point for efficiency sake!
 
-        for edge_cost, neighbor_cheat_node, neighbor_x, neighbor_y in adj_list[node]:
-            # print(f"{neighbor_cheat_node, neighbor_x, neighbor_y=}")
-            neighbor = (neighbor_x, neighbor_y)
-            if neighbor not in visited:
-                # edge_cost = 1 + int(neighbor_cheat_node is not None)
-                assert not (neighbor_cheat_node and cheat_node)
-                new_cheat_node = neighbor_cheat_node if not cheat_node else cheat_node
-                # new_cheat_node = cheat_node if edge_cost != 2 else neighbor
-                if new_cheat_node not in banned:
-                    heapq.heappush(fringe, (cost + edge_cost, neighbor, new_cheat_node))
+        # Add as a neighbor, every square that is at most 20 away from (x, y), and ends at a non-wall position
+        # assert grid[x][y] != WALL
+        queue = collections.deque(
+            [(0, x, y)]
+        )  # (path_len, cheat_x, cheat_y)
+        vis = set()
+        vis.add((x, y))
+        while len(queue) > 0:
+            path_len, cheat_x, cheat_y = queue.popleft()
+            cheat_id = f"{x, y}-->{cheat_x, cheat_y}"
+            if path_len > 20:
+                continue  # Invalid!
+            if (
+                inBounds(cheat_x, cheat_y)
+                and grid[cheat_x][cheat_y] != WALL
+                and (x, y) != (cheat_x, cheat_y) # Since we need to shave off >0 picoseconds!
+            ):  # Last picosecond of cheat can NOT be at a wall!!!
+                # assert cheat_id not in found_cheats
+                full_cost = cost + path_len + bfs(cheat_x, cheat_y)
+                if full_cost <= GOAL_COST:
+                    found_cheats.add(cheat_id)
 
-                # if edge_cost == 2:
-                #     if new_cheat_node not in banned:
-                #         fringe.append((cost + edge_cost, neighbor, new_cheat_node))
-                # else:
-                #     fringe.appendleft((cost + edge_cost, neighbor, new_cheat_node))
+            for dx, dy in DIRECTIONS:
+                new_x, new_y = cheat_x + dx, cheat_y + dy
+                if inBounds(new_x, new_y) and (new_x, new_y) not in vis:
+                    vis.add((new_x, new_y))
+                    queue.append((path_len + 1, new_x, new_y))
 
-    print(f"TERMINATED")
-    exit()
-
-    # fringe = [(True, 0, start[0], start[1])] # (cost, can_disable, x, y)
-    # visited = set()
-    # while len(fringe) > 0:
-    #     can_disable, cost, x, y = heapq.heappop(fringe)
-
-    #     if (x, y) in visited:
-    #         continue
-    #     if (x, y) != GOAL_STATE:
-    #         visited.add((x, y))
-
-    #     if (x, y) == GOAL_STATE and not can_disable:
-    #         # print(f"SHORTEST PATH: {cost}")
-    #         # exit()
-
-    #     for dx, dy in DIRECTIONS:
-    #         new_x, new_y = x + dx, y + dy
-    #         if not inBounds(new_x, new_y):
-    #             continue
-
-    #         if can_disable:
-    #             # heapq.heappush(fringe, (cost + 1, False, new_x, new_y))
-    #             for delta_x, delta_y in DIRECTIONS:
-    #                 cheat_x, cheat_y = new_x + delta_x, new_y + delta_y
-    #                 if inBounds(cheat_x, cheat_y):
-    #                     heapq.heappush(fringe, (False, cost + 2, cheat_x, cheat_y))
-
-    #         if grid[new_x][new_y] != WALL:
-    #             heapq.heappush(fringe, (can_disable, cost + 1, new_x, new_y))
+    print(f"ANSWER: {len(found_cheats)}")
