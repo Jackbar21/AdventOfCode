e0266f385da983533535bea4c9a8c114b9399ab1
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2024-12-22 22:55:15 -0600

    22 - part1 complete

---
+++ 22/part1.py
@@ -0,0 +1,39 @@
+USE_TEST_DATA = False
+
+file_name = "./data.txt" if not USE_TEST_DATA else "./test_data.txt"
+with open(file_name, "r") as file:
+    lines = [int(line.strip()) for line in file.readlines()]
+
+    def getNextSecretNumber(secret_number):
+        # Step 1
+        calc = secret_number * 64
+        secret_number = mix(secret_number, calc)
+        secret_number = prune(secret_number)
+
+        # Step 2
+        calc = secret_number // 32
+        secret_number = mix(secret_number, calc)
+        secret_number = prune(secret_number)
+
+        # Step 3
+        calc = secret_number * 2048
+        secret_number = mix(secret_number, calc)
+        secret_number = prune(secret_number)
+
+        # Finally, return the new secret number!
+        return secret_number
+
+    def mix(num1, num2):
+        return num1 ^ num2
+
+    def prune(num):
+        return num % 16777216
+
+    def getKthSecretNumber(secret_number, k):
+        assert k >= 0
+        for _ in range(k):
+            secret_number = getNextSecretNumber(secret_number)
+        return secret_number
+
+    res = sum(getKthSecretNumber(num, 2000) for num in lines)
+    print(f"ANSWER: {res}")
