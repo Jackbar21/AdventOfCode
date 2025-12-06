93bb453cb8dbe0d3248762ea580e9c44a9b42d6c
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-12-05 16:12:55 -0800

    2025/04/part2

--- 2025/04/part2.py
+++ 2025/04/part2.py
@@ -1,9 +1,43 @@
-
-USE_TEST_DATA = True
+USE_TEST_DATA = False
 
 file_name = "./data.txt" if not USE_TEST_DATA else "./test_data.txt"
 with open(file_name, "r") as file:
     lines = [line.strip() for line in file.readlines()]
+
+    EMPTY, PAPER_ROLL = ".", "@"
+
+    N = len(lines)
+    M = len(lines[0])
+    assert all(len(line) == M for line in lines)
+    grid = [[pos for pos in line] for line in lines]
+
+    inBounds = lambda i, j: 0 <= i < N and 0 <= j < M
+    DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+
+    def countAdjacentPaperRolls(i, j):
+        res = 0
+        for di, dj in DIRECTIONS:
+            neigh_i, neigh_j = i + di, j + dj
+            if inBounds(neigh_i, neigh_j) and grid[neigh_i][neigh_j] == PAPER_ROLL:
+                res += 1
+        return res
+
+    MAX_PAPER_ROLLS = 4
     res = 0
+    l = []
+    repeat = True
+    while repeat:
+        repeat = False
+        for i in range(N):
+            for j in range(M):
+                pos = grid[i][j]
+                if pos != PAPER_ROLL:
+                    continue
+
+                paper_rolls = countAdjacentPaperRolls(i, j)
+                if paper_rolls < MAX_PAPER_ROLLS:
+                    grid[i][j] = EMPTY
+                    res += 1
+                    repeat = True
 
     print(f"ANSWER: {res}")
