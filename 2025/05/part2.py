a003b9808a6045f77f17f0d273fbf29f635e8e43
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-12-05 16:42:35 -0800

    2025/05/part2

--- 2025/05/part2.py
+++ 2025/05/part2.py
@@ -1,4 +1,4 @@
-USE_TEST_DATA = True
+USE_TEST_DATA = False
 from collections import defaultdict
 
 file_name = "./data.txt" if not USE_TEST_DATA else "./test_data.txt"
@@ -6,7 +6,6 @@ with open(file_name, "r") as file:
     lines = [line.strip() for line in file.readlines()]
     index = lines.index("")
     ranges = lines[:index]
-    fresh_ingredients = set()
 
     res = 0
     line_sweep = defaultdict(int)
@@ -16,8 +15,23 @@ with open(file_name, "r") as file:
         start, end = split_range
         start, end = int(start), int(end)
 
-        fresh_ingredients.update(range(start, end + 1))
+        line_sweep[start] += 1
+        line_sweep[end + 1] -= 1
 
-    res = len(fresh_ingredients)
+    sorted_keys = sorted(line_sweep.keys())
+
+    prev_key = None
+    delta = 0
+    res = 0
+    for key in sorted_keys:
+        if prev_key is None:
+            prev_key = key
+
+        delta += line_sweep[key]
+        if delta == 0:
+            # Range of fresh ingredients: prev_key - key (for total of 'key - prev_key' fresh ingredients!)
+            res += key - prev_key
+            prev_key = None
+
 
     print(f"ANSWER: {res}")
