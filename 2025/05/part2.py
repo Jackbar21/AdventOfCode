548ac025377a2a0b87aaddf0ded4a1cf64dff839
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-12-05 16:30:23 -0800

    2025/05/part2 -- too slow

--- 2025/05/part2.py
+++ 2025/05/part2.py
@@ -1,9 +1,23 @@
-
 USE_TEST_DATA = True
+from collections import defaultdict
 
 file_name = "./data.txt" if not USE_TEST_DATA else "./test_data.txt"
 with open(file_name, "r") as file:
     lines = [line.strip() for line in file.readlines()]
+    index = lines.index("")
+    ranges = lines[:index]
+    fresh_ingredients = set()
+
     res = 0
+    line_sweep = defaultdict(int)
+    for r in ranges:
+        split_range = r.split("-")
+        assert len(split_range) == 2
+        start, end = split_range
+        start, end = int(start), int(end)
+
+        fresh_ingredients.update(range(start, end + 1))
+
+    res = len(fresh_ingredients)
 
     print(f"ANSWER: {res}")
