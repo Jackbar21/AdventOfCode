25dea2066bab4e96afec8fdbf166cfaf59dfab42
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-12-10 13:48:43 -0800

    2025/10/part2 -- dp, also too slow

--- 2025/10/part2.py
+++ 2025/10/part2.py
@@ -15,40 +15,46 @@ with open(file_name, "r") as file:
         options = [tuple(int(op) for op in option) for option in options]
 
         joltage_requirements = tuple(map(int, line[-1][1:-1].split(",")))
-        
+
         problems.append((indicator_lights, options, joltage_requirements))
 
-    
+    def dp(i, memo, options, requirements):
+        # Want requirements to be all zeros
+        if all(r == 0 for r in requirements):
+            return 0
+
+        if i >= len(options):
+            return float("inf")
+
+        if (i, requirements) in memo:
+            return memo[(i, requirements)]
+
+        option = options[i]
+
+        # Case 1: skip this option
+        case1 = dp(i + 1, memo, options, requirements)
+
+        # Case 2: use this option (if valid)
+        valid = True
+        new_requirements = list(requirements)
+        for idx in option:
+            new_requirements[idx] -= 1
+            if new_requirements[idx] < 0:
+                valid = False
+                break
+        case2 = (
+            float("inf")
+            if not valid
+            else 1 + dp(i, memo, options, tuple(new_requirements))
+        )
+
+        memo[(i, requirements)] = min(case1, case2)
+        return memo[(i, requirements)]
+
     res = 0
     for _, options, joltage_requirements in problems:
-        # Start at 0 (all lights OFF) and run BFS to find shortest path to indicator_lights
-        initial_voltages = tuple([0] * len(joltage_requirements))
-
-        queue = deque([(0, initial_voltages)])  # (steps, cur_voltages)
-        visited = set([initial_voltages])
-
-        while queue:
-            steps, cur_voltages = queue.popleft()
-            if cur_voltages == joltage_requirements:
-                res += steps
-                print(f"Found solution in {steps} steps")
-                break
-
-            for option in options:
-                next_voltages = list(cur_voltages)
-                valid = True
-                for idx in option:
-                    next_voltages[idx] += 1
-                    if next_voltages[idx] > joltage_requirements[idx]:
-                        valid = False
-                        break
-                
-                if not valid:
-                    continue
-                
-                next_voltages = tuple(next_voltages)
-                if next_voltages not in visited:
-                    visited.add(next_voltages)
-                    queue.append((steps + 1, next_voltages))
+        memo = {}
+        steps = dp(0, memo, options, joltage_requirements)
+        res += steps
 
     print(f"ANSWER: {res}")
