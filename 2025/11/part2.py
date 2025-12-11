b37fbfb7499a7a5c0aae20d41cff28e8204d7d67
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-12-10 21:34:45 -0800

    2025/11/part2

--- 2025/11/part2.py
+++ 2025/11/part2.py
@@ -1,9 +1,42 @@
+USE_TEST_DATA = False
+from collections import defaultdict, deque
 
-USE_TEST_DATA = True
-
-file_name = "./data.txt" if not USE_TEST_DATA else "./test_data.txt"
+file_name = "./data.txt" if not USE_TEST_DATA else "./test_data2.txt"
 with open(file_name, "r") as file:
     lines = [line.strip() for line in file.readlines()]
     res = 0
 
+    d = {
+        line[0]: line[1].split(" ")
+        for line in map(lambda line: line.split(": "), lines)
+    }
+    adj_list = defaultdict(set)
+    for src, dests in d.items():
+        for dest in dests:
+            adj_list[src].add(dest)
+
+    START, GOAL = "svr", "out"
+
+    memo = {}
+    def dp(node, dac, fft):
+        if (node, dac, fft) in memo:
+            return memo[(node, dac, fft)]
+
+        if node == "dac":
+            dac = True
+
+        if node == "fft":
+            fft = True
+
+        if node == GOAL:
+            return 1 if dac and fft else 0
+
+        total = 0
+        for neighbor in adj_list[node]:
+            total += dp(neighbor, dac, fft)
+
+        memo[(node, dac, fft)] = total
+        return total
+
+    res = dp(START, False, False)
     print(f"ANSWER: {res}")
