7003a938dc342458ed7bd634b3693d51a9ec5122
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2025-12-11 21:53:23 -0800

    2025: delete 13-25

--- 2025/19/part2.py
+++
@@ -1,9 +0,0 @@
-
-USE_TEST_DATA = True
-
-file_name = "./data.txt" if not USE_TEST_DATA else "./test_data.txt"
-with open(file_name, "r") as file:
-    lines = [line.strip() for line in file.readlines()]
-    res = 0
-
-    print(f"ANSWER: {res}")
